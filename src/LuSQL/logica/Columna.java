7c403dd02773e58da8fd6ccc66246e8bf2c09e9d
Administrador <Administrador@Hugo-PC>
2017-07-28 15:57:51 +0200

    28/07/2017 - Completada parte lógica

--- src/LuSQL/logica/Columna.java
+++ src/LuSQL/logica/Columna.java
@@ -1,16 +1,26 @@
 package LuSQL.logica;
 
+/**
+ * Objecto que almacena información sobre una columna de bases de datos.
+ * Solo almacena información sobre como esta formada la columna, no almacena ningún tipo de valor
+ */
 public class Columna {
     
     private String nombre;
     private String tipo;
+    private String longitudValores;
+    private boolean primaryKey;
+    private boolean autoIncremental;
+    private boolean nulo;
 
-    public Columna() {
+    public Columna() {   
+        this.longitudValores="";
     }
 
     public Columna(String nombre, String tipo) {
         this.nombre = nombre;
         this.tipo = tipo;
+        this.longitudValores="";
     }   
 
     public String getNombre() {
@@ -28,7 +38,58 @@ public class Columna {
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-    
-    
+
+    public String getLongitudValores() {
+        return longitudValores;
+    }
+
+    public void setLongitudValores(String longitudValores) {
+        this.longitudValores = longitudValores;
+    }
+
+    public boolean isPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(boolean primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    public boolean isAutoIncremental() {
+        return autoIncremental;
+    }
+
+    public void setAutoIncremental(boolean autoIncremental) {
+        this.autoIncremental = autoIncremental;
+    }
+
+    public boolean isNulo() {
+        return nulo;
+    }
+
+    public void setNulo(boolean nulo) {
+        this.nulo = nulo;
+    }
+
+    @Override
+    public String toString() {
+        String pKey="";
+        String nulo="";
+        String AI="";
+        String longValores="";
+        if(isPrimaryKey()){
+            pKey=" PRIMARY KEY";
+        } 
+        if(!isNulo()){
+            nulo=" NOT NULL";
+        }
+        if(!longitudValores.isEmpty()){
+            longValores="("+longitudValores+")";
+        }
+        if(isAutoIncremental()){
+            AI=" AUTO_INCREMENT";
+        }
+        return this.nombre+" "+this.tipo+longValores+pKey+AI+nulo; 
+    }
     
 }
--- src/LuSQL/logica/ConexionBD.java
+++ src/LuSQL/logica/ConexionBD.java
@@ -5,13 +5,14 @@ import LuSQL.logica.Interfaces.BaseDatos;
 import LuSQL.logica.Interfaces.Tabla;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
  * Clase que nos permite trabajar con las base de datos de un servidorSQL
  */
-
 
   //TODO los métodos de obtener tablas de momento solo funcionan para sqlserver 
 public class ConexionBD implements BaseDatos{
@@ -57,31 +58,113 @@ public class ConexionBD implements BaseDatos{
     }
 
     @Override
-    public boolean crearNuevaTabla(Tabla tabla) {
-        return false;
+    public boolean crearNuevaTabla(String nombreTabla,Columna[] columnas) {  
+        boolean result;
+        Statement statement;
+        try {
+            String sentenciaSQL="CREATE TABLE "+nombreTabla+
+                    " (" + getSentenciaCrearTablaColumnas(columnas) + ");";                                 
+            statement=servidor.getConexion().createStatement();
+            statement.execute(sentenciaSQL); 
+            statement.close();
+            result=true;
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());  
+            result=false;
+        }finally{
+            statement=null;
+        }
+        return result;
+    }
+    
+    private String getSentenciaCrearTablaColumnas(Columna[] columnas){
+        String sentenciaColumnas="";         
+            for (int i = 0; i < columnas.length; i++) {                
+                if(i==columnas.length-1){
+                    sentenciaColumnas+=columnas[i].toString();
+                }else{
+                    sentenciaColumnas+=columnas[i].toString()+",";
+                }
+            } 
+            return sentenciaColumnas;        
     }
 
     @Override
     public boolean eliminarTabla(String nombreTabla) {
-        return false;
+        Statement statement;        
+        boolean resultado;
+        try {           
+            statement=servidor.getConexion().createStatement();
+            statement.execute("DROP TABLE "+nombreTabla);
+            statement.close();
+            resultado=true;
+        } catch (Exception ex) {
+            resultado=false;
+        }finally{
+            statement=null;       
+        }        
+        return resultado;
     }
 
     @Override
-    public void ejecutarSentencia(String sentencia) throws SQLException{
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void ejecutarSentencia(String sentencia) throws SQLException{        
+        Statement statement=servidor.getConexion().createStatement();
+        statement.execute(sentencia);
+        statement.close();
     }
 
     @Override
     public Tabla ejecutarConsulta(String consulta) {
-        return null;
+        Tabla result=null;
+        Statement statement;
+        ResultSet rsConsulta;
+        ResultSetMetaData rsMetaData;
+        try {
+            result=new ConexionTabla(this,"Resultado",true);            
+            statement=servidor.getConexion().createStatement();
+            rsConsulta=statement.executeQuery(consulta);
+            rsMetaData = rsConsulta.getMetaData();
+            //añadimos columnas y filas a la tabla
+            result.setColumnas(getColumnasConsulta(rsMetaData));
+            result.setListValores(getValoresConsulta(rsConsulta,result.totalColumnas()));
+            statement.close();
+            rsConsulta.close();           
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());
+            result=null;
+        }finally{          
+            statement=null;
+            rsConsulta=null;          
+        }
+        return result;
+    }
+    //TODO, solo obtenemos el nombre y tipo de columna, faltan añadir mas datos
+    private Columna[] getColumnasConsulta(ResultSetMetaData rsMetaData) throws SQLException{
+        Columna[] resultado=new Columna[rsMetaData.getColumnCount()];        
+            for (int i = 0; i < resultado.length; i++) {
+                resultado[i]=new Columna(rsMetaData.getColumnName(i+1),rsMetaData.getColumnTypeName(i+1));                 
+            }
+            return resultado;
+    }
+    
+    private List<String[]> getValoresConsulta(ResultSet rsConsulta, int numColumnas) throws SQLException {
+        List<String[]> resultado = new ArrayList();
+        String valores[];
+        while (rsConsulta.next()) {
+            valores = new String[numColumnas];
+            for (int i = 0; i < numColumnas; i++) {
+                valores[i] = rsConsulta.getString(i + 1);
+            }
+            resultado.add(valores);
+        }
+        return resultado;
     }
 
     @Override
     public ServidorSQL getServidor() {
         return servidor;
     }
-
-   //Metodos privados
+ 
     private DatabaseMetaData getMetaData() throws SQLException{
         if(metadata==null){
             metadata=servidor.getConexion().getMetaData();
